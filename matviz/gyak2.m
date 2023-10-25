e3903fee220a651654bc1eb0facafa00a57fd551
Balic Erik <85021283+erikbalic1@users.noreply.github.com>
2023-10-25 09:37:11 +0200

    Update gyak2.m

--- matviz/gyak2.m
+++ matviz/gyak2.m
@@ -53,3 +53,13 @@ z(u, v) = v;
 m = 5;
 fsurf(x, y, z, [0 2*pi 0 m])
 axis equal
+
+
+clear
+syms u v 
+R = 5; r = 2;
+x(u, v) = (R + r * cos(v)) * cos(u);
+y(u, v) = (R + r * cos(v)) * sin(u);
+z(u, v) = r * sin(v);
+fsurf(x, y, z, [0 2*pi 0 2*pi])
+axis equal
