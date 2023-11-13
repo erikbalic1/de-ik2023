a197d0df8b2634478a94b82fa87878bc02483d14
Balic Erik <85021283+erikbalic1@users.noreply.github.com>
2023-11-13 17:09:51 +0100

    Update fibonacci.py

--- bevprog/fibonacci.py
+++ bevprog/fibonacci.py
@@ -1,11 +1,11 @@
-def fibonacci(n):
-    if n == 1:
-        return 1
-    elif n == 2:
+def fibonacci(n): # deklaraljuk a fuggvenyt
+    if n == 1: # megnezzuk az elso elemre
+        return 1 
+    elif n == 2: # megnezzuk a masodik elemre
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2) # kiszamoljuk az n-edik tagod fibonacci keplete szerint
 def main():
-    print(fibonacci(6))
+    print(fibonacci(6)) 
 if __name__ == "__main__":
     main()
