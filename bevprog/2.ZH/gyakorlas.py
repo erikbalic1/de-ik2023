a62ae2e4a0eeee4056bb62d957bca1bd92a68fe5
Balic Erik <85021283+erikbalic1@users.noreply.github.com>
2023-11-27 16:48:55 +0100

    Update gyakorlas.py

--- bevprog/2.ZH/gyakorlas.py
+++ bevprog/2.ZH/gyakorlas.py
@@ -1,4 +1,4 @@
-# 1) szumma1 fuggveny: {i = 1}^n i*(i^2-1)
+# 1) szumma1 fuggveny: 1-tol n-ig -> i*(i^2 - 1)
 def szumma1(n):
     if n < 3:
       return "Nem megfelelo adat"
@@ -9,5 +9,19 @@ def szumma1(n):
 def main():
     n = int(input("Vegertek: "))
     print(szumma1(n))
+if __name__ == "__main__":
+    main()
+#2) szumma2 fuggveny: k-tol indul n-ig -> i*(i^2 - 1)
+def szumma2(k, n):
+    if n < k:
+        return "Nem megfelelo adat"
+    elif n == k:
+        return n * (n * n - 1)
+    else:
+        return  n * (n * n - 1) + szumma2(k, n - 1)
+def main():
+    k = int(input("Kezdoertek: "))
+    n = int(input("Vegertek: "))
+    print(szumma2(k, n))
 if __name__ == "__main__":
     main()
